7736f79a7accba46745b150759f13192ba105f13
Andrés Garbarz <andresgarbarz@gmail.com>
2022-08-30 14:58:52 -0300

    Generación de Tablero
    .

--- juego.py
+++ juego.py
@@ -22,9 +22,32 @@ def rellenar_tablero(tablero):
             num = randint(2, 12)
         else:
             n_used[pos] = num
+            tablero.colocar_numero(pos+1, num)
             num = randint(2, 12)
             pos = randint(1, 19) - 1
     print(n_used)
+
+    m_used = [None]*19
+    materials = ["Ladrillo", "Piedra", "Trigo", "Lana", "Madera"]
+    three = ["Ladrillo", "Piedra"]
+    four = ["Trigo", "Lana", "Madera"]
+    mat = materials[randint(1, 5) - 1]
+    pos = randint(1, 19) - 1
+    while None in m_used:
+        if pos == 9:
+           m_used[pos] = ""
+           pos = randint(1, 19) - 1
+        elif mat in three and m_used.count(mat) >= 3:
+            mat = materials[randint(1, 5) - 1]
+        elif mat in four and m_used.count(mat) >= 4:
+            mat = materials[randint(1, 5) - 1]
+        else:
+            m_used[pos] = mat
+            tablero.colocar_recurso(pos+1, mat)
+            mat = materials[randint(1, 5) - 1]
+            pos = randint(1, 19) - 1
+    m_used[9] = None
+    print(m_used)        
 
 def jugar_catan(jugadores,tablero):
     pass
--- juego_con_interfaz.py
+++ juego_con_interfaz.py
@@ -1,8 +1,11 @@
 from interfaz import jugar_con_interfaz
+from tablero import TableroCatan
+from juego import rellenar_tablero
 
 #Lista de jugadores
 jugadores = []
 #Tablero
-tablero_a_jugar = None
+tablero_a_jugar = TableroCatan()
+rellenar_tablero(tablero_a_jugar)
 #No se olviden de rellenar el tablero!
 jugar_con_interfaz(jugadores,tablero_a_jugar)
